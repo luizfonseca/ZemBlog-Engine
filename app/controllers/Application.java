8354e6fd975163e6b92d8bef904f542ee8dd0a12
Luiz Fonseca <runeroniek@gmail.com>
2011-07-28 10:41:57 -0400

    New bootstrap file and layout template

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,15 +2,16 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import java.util.*;
-
 import models.*;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		Post frontPost = Post.find("order by postedAt desc").first();
+		System.out.println(Post.count() + " posts were found");
+		System.out.println(User.count() + " user were found");
+		render(frontPost);
+	}
 
 }
